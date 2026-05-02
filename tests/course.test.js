364c9a8302116ba0152c97092334e4b2378ed6ce
Oscar Lin <Oscar.Lin@quinnipiac.edu>
2026-05-02 00:01:06 -0400

    merge???

--- tests/course.test.js
+++ tests/course.test.js
@@ -14,6 +14,18 @@ describe('Course', () => {
     test('sets credits', () => expect(course.credits).toBe(3));
   });
 
+  describe('negative credits', () => {
+    test('stores negative credits as-is', () => {
+      const c = new Course('CS', 101, 'Intro to Programming', -3);
+      expect(c.credits).toBe(-3);
+    });
+
+    test('toString reflects negative credits', () => {
+      const c = new Course('CS', 101, 'Intro to Programming', -3);
+      expect(c.toString()).toBe('Intro to Programming, CS 101 (-3 credits)');
+    });
+  });
+
   describe('toString', () => {
     test('returns formatted course string', () => {
       expect(course.toString()).toBe('Intro to Programming, CS 101 (3 credits)');
